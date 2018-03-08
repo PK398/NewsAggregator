a68b4dd137ff739069f8905b2460a7b13e7c6adb
Pranav Kalidindi <pvk16@ic.ac.uk>
2018-03-08 11:11:38 +0000

    Parse Guardian RSS feed

---
+++ RSS-model/src/main/java/RSS/NewsAggregator/NewsSources/Guardian.java
@@ -0,0 +1,25 @@
+package RSS.NewsAggregator.NewsSources;
+
+import org.jdom2.Element;
+import org.jdom2.Namespace;
+
+public class Guardian extends RSSParser {
+
+  @Override
+  public String getTopic(String topic) {
+    return null;
+  }
+
+  @Override
+  public String getImage(Element story) {
+    Element image = story
+        .getChild("content", Namespace.getNamespace("http://search.yahoo.com/mrss/"));
+    return image.getAttributeValue("url");
+  }
+
+  @Override
+  public String getAuthor(Element story) {
+    Element author = story.getChild("creator", Namespace.getNamespace("http://purl.org/dc/elements/1.1/"));
+    return author.getText();
+  }
+}
